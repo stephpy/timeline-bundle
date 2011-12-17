1f8cc5a8a4a964ec0db8414541824fd0cb043590
Stéphane PY <py.stephane1@gmail.com>
2011-12-17 22:32:08 +0100

    Add abstract class for my test using doctrine, i create a doctrine connection in this class, because if user has no configured his config_test.yml, i dont want to break his app

---
+++ Tests/AbstractDoctrineConnectionTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Highco\TimelineBundle\Tests;
+
+require_once __DIR__.'/../../../../../app/AppKernel.php';
+
+use Doctrine\ORM\Tools\SchemaTool;
+
+class AbstractDoctrineConnectionTest extends \PHPUnit_Framework_TestCase
+{
+	protected $kernel;
+
+	/**
+     * setUp
+     *
+     * @access public
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->kernel = new \AppKernel('test', true);
+        $this->kernel->boot();
+
+        $this->container = $this->kernel->getContainer();
+
+		// this step is for users which not update their config_test.yml :/
+		// @todo, let user choice to use his config_test.yml connection
+        $conn = $this->container->get('doctrine.dbal.connection_factory')->createConnection(array(
+            'dbname' => 'highco_timeline_test',
+            'host' => 'localhost',
+            'port' => NULL,
+            'user' => 'root',
+            'password' => 'root',
+            'driver' => 'pdo_sqlite',
+            'charset' => 'UTF8',
+            'memory' => true,
+            'driverOptions' => array()
+        ), null, new \Doctrine\Common\EventManager(), array());
+
+        $this->container->set('doctrine.dbal.default_connection', $conn);
+
+        $this->em        = $this->container->get('doctrine')->getEntityManager();
+
+        $this->generateSchema();
+
+        parent::setUp();
+    }
+
+    /**
+     * tearDown
+     *
+     * @access public
+     * @return void
+     */
+    public function tearDown()
+    {
+        // Shutdown the kernel.
+        $this->kernel->shutdown();
+
+        parent::tearDown();
+    }
+
+    /**
+     * generateSchema
+     *
+     * @access protected
+     * @return void
+     */
+    protected function generateSchema()
+    {
+        // Get the metadatas of the application to create the schema.
+        $metadatas = $this->getMetadatas();
+
+        if ( ! empty($metadatas))
+        {
+            $tool = new SchemaTool($this->em);
+            $tool->createSchema($metadatas);
+        }
+        else
+        {
+            throw new Doctrine\DBAL\Schema\SchemaException('No Metadata Classes to process.');
+        }
+    }
+
+    /**
+     * Overwrite this method to get specific metadatas.
+     *
+     * @return Array
+     */
+    protected function getMetadatas()
+    {
+        return $this->em->getMetadataFactory()->getAllMetadata();
+    }
+
+}
