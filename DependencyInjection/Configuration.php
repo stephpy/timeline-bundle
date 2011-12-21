eae6ea9573f6afa74805f96e345932b5572dd5cc
Stéphane PY <py.stephane1@gmail.com>
2011-12-21 17:50:47 +0100

    Fix indent on configuration + indirect complement must be nullable

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -25,32 +25,31 @@ class Configuration implements ConfigurationInterface
     {
         $tb = new TreeBuilder();
 
-		$tb->root('highco_timeline')
+        $tb->root('highco_timeline')
             ->children()
                 ->arrayNode('filters')
-					->useAttributeAsKey('options')->prototype('scalar')->end()
-					->defaultValue(array(
-						'highco.timeline.filter.dupplicate_key',
-						'highco.timeline.filter.data_hydrator',
-					))
-				->end()
-			->end()
-			->children()
-				->arrayNode('spread')
-					->children()
-						->scalarNode('on_me')->defaultValue(true)->end()
-						->scalarNode('on_global_context')->defaultValue(true)->end()
-					->end()
-				->end()
-			->end()
-            ->children()
-				->scalarNode('provider')->defaultValue('highco.timeline.provider.redis')->end()
+                    ->useAttributeAsKey('options')->prototype('scalar')->end()
+                    ->defaultValue(array(
+                        'highco.timeline.filter.dupplicate_key',
+                        'highco.timeline.filter.data_hydrator',
+                    ))
+                ->end()
             ->end()
             ->children()
-				->scalarNode('delivery')->defaultValue('immediate')->end()
+                ->arrayNode('spread')
+                    ->children()
+                        ->scalarNode('on_me')->defaultValue(true)->end()
+                        ->scalarNode('on_global_context')->defaultValue(true)->end()
+                    ->end()
+                ->end()
+            ->end()
+            ->children()
+                ->scalarNode('provider')->defaultValue('highco.timeline.provider.redis')->end()
+            ->end()
+            ->children()
+                ->scalarNode('delivery')->defaultValue('immediate')->end()
             ->end()
             ;
-
 
         return $tb;
     }
