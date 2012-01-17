36f1d013aeebeb6d1b2a07d7c2a24b06fffece09
Stéphane PY <py.stephane1@gmail.com>
2012-01-17 17:12:46 +0100

    exceed doctrine orm proxy on timeline action

--- Entity/TimelineAction.php
+++ Entity/TimelineAction.php
@@ -14,4 +14,60 @@ use Highco\TimelineBundle\Model\TimelineAction as BaseTimelineAction;
  */
 class TimelineAction extends BaseTimelineAction
 {
+    /**
+     * setSubjectModel
+     *
+     * @param string $v
+     * @return void
+     */
+    public function setSubjectModel($v)
+    {
+        return parent::setSubjectModel($this->exceedDoctrineORMProxy($v));
+    }
+
+    /**
+     * setDirectComplementModel
+     *
+     * @param string $v
+     * @return void
+     */
+    public function setDirectComplementModel($v)
+    {
+        return parent::setDirectComplementModel($this->exceedDoctrineORMProxy($v));
+    }
+
+    /**
+     * setIndirectComplementModel
+     *
+     * @param string $v
+     * @return void
+     */
+    public function setIndirectComplementModel($v)
+    {
+        return parent::setIndirectComplementModel($this->exceedDoctrineORMProxy($v));
+    }
+
+    /**
+     * exceedDoctrineORMProxy
+     *
+     * Because proxy of doctrine ORM are boring and spread supports will be impacted if we do not use it.
+     *
+     * @param string $class
+     * @return string
+     */
+    public function exceedDoctrineORMProxy($class)
+    {
+        if(empty($class))
+        {
+            return $class;
+        }
+
+        $reflectionClass = new \ReflectionClass($class);
+        if($reflectionClass->implementsInterface('\Doctrine\ORM\Proxy\Proxy'))
+        {
+            $reflectionClass = $reflectionClass->getParentClass();
+        }
+
+        return $reflectionClass->getName();
+    }
 }
