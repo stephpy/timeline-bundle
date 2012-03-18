c2400d0009e9e3f21b945efd0138f1e645e10c5b
Stéphane PY <py.stephane1@gmail.com>
2012-03-18 21:57:57 +0100

    Test command

--- Tests/Command/DeployTimelineActionCommandTest.php
+++ Tests/Command/DeployTimelineActionCommandTest.php
@@ -5,23 +5,38 @@ namespace Highco\TimelineBundle\Tests\Command;
 use Highco\TimelineBundle\Entity\TimelineAction;
 
 use Symfony\Component\Console\Tester\CommandTester;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Application;
 use Highco\TimelineBundle\Command\DeployTimelineActionCommand;
+use Symfony\Component\DependencyInjection\Container;
 
+/**
+ * DeployTimelineActionCommandTest
+ *
+ * @author Stephane PY <py.stephane1@gmail.com>
+ */
 class DeployTimelineActionCommandTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * testNoTimeline
+     */
     public function testNoTimeline()
     {
-        //@todo use mock instead of use real connection !
-        /*$results = $this->em->getRepository('HighcoTimelineBundle:TimelineAction')
-            ->createQueryBuilder('t')
-            ->getQuery()
-            ->getResult();
+        $manager = $this->getMock('\Highco\TimelineBundle\Entity\TimelineActionManager', array(), array(), '', false);
+        $manager->expects($this->once())
+            ->method('getTimelineWithStatusPublished')
+            ->will($this->returnValue(array()));
 
-        $this->assertEquals(count($results), 0);
+        $deployer = $this->getMock('\Highco\TimelineBundle\Timeline\Spread\Deployer', array(), array(), '', false);
 
-        $application = new Application($this->client->getKernel());
-        $application->add(new DeployTimelineActionCommand());
+        $container = new Container();
+        $container->set('highco.timeline_action_manager', $manager);
+        $container->set('highco.timeline.spread.deployer', $deployer);
+
+        $command = new DeployTimelineActionCommand();
+        $command->setContainer($container);
+
+        $application = new Application();
+        $application->add($command);
 
         $command = $application->find('highco:timeline-deploy');
         $commandTester = new CommandTester($command);
@@ -32,28 +47,37 @@ class DeployTimelineActionCommandTest extends \PHPUnit_Framework_TestCase
         $result  = "There is 0 timeline action(s) to deploy\n";
         $result .= "Done\n";
 
-        $this->assertEquals($display, $result);*/
+        $this->assertEquals($display, $result);
     }
 
+    /**
+     * testOneTimeline
+     */
     public function testOneTimeline()
     {
-        //@todo use mock instead of use real connection !
-        /*$this->createTimelineAction(1);
+        $timelineAction = $this->getMock('\Highco\TimelineBundle\Entity\TimelineAction');
+        $timelineAction->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue(1));
 
-        $results = $this->em->getRepository('HighcoTimelineBundle:TimelineAction')
-            ->createQueryBuilder('t')
-            ->getQuery()
-            ->getResult();
 
-        $this->assertEquals(count($results), 1);
-        foreach ($results as $result)
-        {
-            $this->assertEquals($result->getStatusWanted(), TimelineAction::STATUS_PUBLISHED);
-            $this->assertEquals($result->getStatusCurrent(), TimelineAction::STATUS_PENDING);
-        }
+        $manager = $this->getMock('\Highco\TimelineBundle\Entity\TimelineActionManager', array(), array(), '', false);
+        $manager->expects($this->once())
+            ->method('getTimelineWithStatusPublished')
+            ->will($this->returnValue(array($timelineAction)));
 
-        $application = new Application($this->client->getKernel());
-        $application->add(new DeployTimelineActionCommand());
+
+        $deployer = $this->getMock('\Highco\TimelineBundle\Timeline\Spread\Deployer', array(), array(), '', false);
+
+        $container = new Container();
+        $container->set('highco.timeline_action_manager', $manager);
+        $container->set('highco.timeline.spread.deployer', $deployer);
+
+        $command = new DeployTimelineActionCommand();
+        $command->setContainer($container);
+
+        $application = new Application();
+        $application->add($command);
 
         $command = $application->find('highco:timeline-deploy');
         $commandTester = new CommandTester($command);
@@ -66,106 +90,5 @@ class DeployTimelineActionCommandTest extends \PHPUnit_Framework_TestCase
         $result .= "Done\n";
 
         $this->assertEquals($display, $result);
-
-        $results = $this->em->getRepository('HighcoTimelineBundle:TimelineAction')
-            ->createQueryBuilder('t')
-            ->getQuery()
-            ->getResult();
-
-        $this->assertEquals(count($results), 1);
-        foreach ($results as $result)
-        {
-            $this->assertEquals($result->getStatusWanted(), TimelineAction::STATUS_FROZEN);
-            $this->assertEquals($result->getStatusCurrent(), TimelineAction::STATUS_PUBLISHED);
-        }*/
-    }
-
-    public function testTwoTimelineWithLimit()
-    {
-        //@todo use mock instead of use real connection !
-        /*$this->createTimelineAction(2);
-
-        $results = $this->em->getRepository('HighcoTimelineBundle:TimelineAction')
-            ->createQueryBuilder('t')
-            ->getQuery()
-            ->getResult();
-
-        $this->assertEquals(count($results), 2);
-        foreach ($results as $result)
-        {
-            $this->assertEquals($result->getStatusWanted(), TimelineAction::STATUS_PUBLISHED);
-            $this->assertEquals($result->getStatusCurrent(), TimelineAction::STATUS_PENDING);
-        }
-
-        $application = new Application($this->client->getKernel());
-        $application->add(new DeployTimelineActionCommand());
-
-        $command = $application->find('highco:timeline-deploy');
-        $commandTester = new CommandTester($command);
-        $commandTester->execute(array('command' => $command->getName(), '--limit' => 1));
-
-        $display = $commandTester->getDisplay();
-
-        $result  = "There is 1 timeline action(s) to deploy\n";
-        $result .= "Deploy timeline action 1\n";
-        $result .= "Done\n";
-
-        $this->assertEquals($display, $result);
-
-        $results = $this->em->getRepository('HighcoTimelineBundle:TimelineAction')
-            ->createQueryBuilder('t')
-            ->getQuery()
-            ->getResult();
-
-        $this->assertEquals(count($results), 2);
-        $first = array_shift($results);
-        $this->assertEquals($first->getStatusWanted(), TimelineAction::STATUS_FROZEN);
-        $this->assertEquals($first->getStatusCurrent(), TimelineAction::STATUS_PUBLISHED);
-
-        $second = array_shift($results);
-        $this->assertEquals($second->getStatusWanted(), TimelineAction::STATUS_PUBLISHED);
-        $this->assertEquals($second->getStatusCurrent(), TimelineAction::STATUS_PENDING);
-         */
-    }
-
-    /**
-     * createTimelineAction
-     *
-     * @param int $howMany
-     * @access protected
-     * @return void
-     */
-    protected function createTimelineAction($howMany = 1)
-    {
-        //@todo use mock instead of use real connection !
-        /*for($i = 0; $i < (int) $howMany; $i++)
-        {
-            $stub = $this->getMock('Highco\TimelineBundle\Tests\Command\EntityStub');
-            $stub->expects($this->once())
-                ->method('getId')
-                ->will($this->returnValue(1));
-
-            $entry = new TimelineAction();
-            $entry->setSubject($stub);
-
-            $entry->setVerb('Own');
-
-            $stub = $this->getMock('Highco\TimelineBundle\Tests\Command\EntityStub');
-            $stub->expects($this->once())
-                ->method('getId')
-                ->will($this->returnValue(2));
-
-            $entry->setDirectComplement($stub);
-
-            $stub = $this->getMock('Highco\TimelineBundle\Tests\Command\EntityStub');
-            $stub->expects($this->once())
-                ->method('getId')
-                ->will($this->returnValue(3));
-            $entry->setIndirectComplement($stub);
-
-            $this->em->persist($entry);
-        }
-
-        $this->em->flush();*/
     }
 }
