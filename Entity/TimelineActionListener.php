6263a4542a2497fd939af80e9546e583f468d1d7
Stéphane PY <py.stephane1@gmail.com>
2012-06-18 23:50:41 +0200

    ref #13, if entity does not exists, not throw an exception

---
+++ Entity/TimelineActionListener.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Highco\TimelineBundle\Entity;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\EntityManager;
+use Highco\TimelineBundle\Model\TimelineActionInterface;
+
+/**
+ * TimelineActionListener
+ *
+ * @uses EventSubscriber
+ * @author Francisco Facioni <fran6co@gmail.com>
+ */
+class TimelineActionListener implements EventSubscriber
+{
+    /**
+     * @param LifecycleEventArgs $eventArgs eventArgs
+     */
+    public function postLoad(LifecycleEventArgs $eventArgs)
+    {
+        $entity = $eventArgs->getEntity();
+
+        if ($entity instanceof TimelineActionInterface) {
+            $em = $eventArgs->getEntityManager();
+
+            if (null !== $entity->getSubjectModel() && null !== $entity->getSubjectId()) {
+
+                try {
+                    $entity->setSubject($em->getReference($entity->getSubjectModel(), $entity->getSubjectId()));
+                } catch (\Exception $e) {
+                }
+            }
+
+            if (null !== $entity->getDirectComplementModel() && null !== $entity->getDirectComplementId()) {
+
+                try {
+                    $entity->setDirectComplement($em->getReference($entity->getDirectComplementModel(), $entity->getDirectComplementId()));
+                } catch (\Exception $e) {
+                }
+            }
+
+            if (null !== $entity->getIndirectComplementModel() && null !== $entity->getIndirectComplementId()) {
+
+                try {
+                    $entity->setIndirectComplement($em->getReference($entity->getIndirectComplementModel(), $entity->getIndirectComplementId()));
+                } catch (\Eception $e) {
+                }
+            }
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getSubscribedEvents()
+    {
+        return array('postLoad');
+    }
+}
