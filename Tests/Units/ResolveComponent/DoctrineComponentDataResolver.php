5ef259a1385f4b5a1a861d58e67a08cd823689e9
michiel <boeckaert@gmail.com>
2014-04-25 18:48:33 +0200

    more tests for doctrine component data resolver

--- Tests/Units/ResolveComponent/DoctrineComponentDataResolver.php
+++ Tests/Units/ResolveComponent/DoctrineComponentDataResolver.php
@@ -54,4 +54,59 @@ class DoctrineComponentDataResolver extends Test
             )
             ;
     }
+
+    public function testObjectNotManagedByDoctrine()
+    {
+        $object = new User(5);
+        $resolve = new ResolveComponentModelIdentifier($object);
+
+        $this->if($this->mockClass('\Doctrine\Common\Persistence\ManagerRegistry', '\Mock'))
+            ->and($managerRegistry = new \Mock\ManagerRegistry())
+            ->and($resolver = new TestedModel())
+            ->and($resolver->addRegistry($managerRegistry))
+            ->when($result = $resolver->resolveComponentData($resolve))
+            ->then(
+                $this->mock($managerRegistry)->call('getManagerForClass')->withArguments('Spy\Timeline\ResolveComponent\TestHelper\User')->exactly(1)
+                ->and($this->string($result->getIdentifier())->isEqualTo(5))
+                ->and($this->string($result->getModel())->isEqualTo('Spy\Timeline\ResolveComponent\TestHelper\User'))
+                )
+        ;
+    }
+
+    public function testObjectNotManagedByDoctrineWithoutGetIdMethod()
+    {
+        $object = new \stdClass();
+        $resolve = new ResolveComponentModelIdentifier($object);
+
+
+        $this->if($this->mockClass('\Doctrine\Common\Persistence\ManagerRegistry', '\Mock'))
+            ->and($managerRegistry = new \Mock\ManagerRegistry())
+            ->and($resolver = new TestedModel())
+            ->and($resolver->addRegistry($managerRegistry))
+            ->exception(function () use ($resolver, $resolve) {
+                $resolver->resolveComponentData($resolve);
+            })
+            ->isInstanceOf('Spy\Timeline\Exception\ResolveComponentDataException')
+            ->hasMessage('Model must have a getId method.')
+            ;
+    }
+
+    public function testStringModelAndIdentifierGiven()
+    {
+        $model = 'foo';
+        $identifier = array('foo' => 'bar');
+        $resolve = new ResolveComponentModelIdentifier($model, $identifier);
+
+        $this->if($this->mockClass('\Doctrine\Common\Persistence\ManagerRegistry', '\Mock'))
+            ->and($managerRegistry = new \Mock\ManagerRegistry())
+            ->and($resolver = new TestedModel())
+            ->and($resolver->addRegistry($managerRegistry))
+            ->when($result = $resolver->resolveComponentData($resolve))
+            ->then(
+                $this->mock($managerRegistry)->call('getManagerForClass')->withArguments('Spy\Timeline\ResolveComponent\TestHelper\User')->exactly(0)
+                ->and($this->array($result->getIdentifier())->isEqualTo($identifier))
+                ->and($this->string($result->getModel())->isEqualTo($model))
+            )
+        ;
+    }
 }
